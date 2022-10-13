976387467d46b9ccc3ee9130535997118e27e5e7
Ishwarivijayjagtap <ishwarijagtap20701@gmail.com>
2022-10-13 08:48:08 +0530

    Start with flipping a coin to display Heads or Tails

--- FlipCoinSimulation.sh
+++ FlipCoinSimulation.sh
@@ -1,4 +1,20 @@
 
-echo ""
-echo "Welcome to Flip-Coin Simulation Problem"
-echo ""
+  echo ""
+   echo "Welcome to Flip-Coin Simulation Problem"
+   echo ""
+
+   Result=$((RANDOM%2))
+
+ if [[ ${Result} -eq 0 ]];
+  then
+    echo "HEADS"
+  elif [[ ${Result} -eq 1 ]]
+ then
+    echo "TAILS"
+fi
+
+
+
+
+
+
